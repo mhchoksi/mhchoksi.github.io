e0dbf7f91c8af98e42b333254c97d52232c1148a
mhchoksi <meghavichoksi@gmail.com>
2022-06-08 17:00:08 -0400

    Update ik_accordion.js

--- assets/ik_accordion.js
+++ assets/ik_accordion.js
@@ -33,32 +33,95 @@
 		plugin = this;
 		
 		$elem.attr({
-			'id': id
+			 'id': id,
+			 'role': 'region' // add the accordion to the landmarked regions
 		}).addClass('ik_accordion');
+		
+		$elem.attr({'aria-multiselectable': !this.options.autoCollapse}); // define if more than one panel can be expanded
 			
-		this.headers = $elem.children('dt').each(function(i, el) {
+		this.headers = $elem.children('dt')
+		.attr({
+			'role': 'heading'
+		})
+		//.attr({'role': 'presentation'})
+		.each(function(i, el) {
 			var $me, $btn;
 			
 			$me = $(el);
 			$btn = $('<div/>').attr({
-          'id': id + '_btn_' + i
+				'id': id + '_btn_' + i,
+			    'role': 'button',
+			    'aria-controls': id + '_panel_' + i, // associate button with corresponding panel
+			    'aria-expanded': false, // toggle expanded state
+			    'tabindex': 0 //add keyboard focus
         })
         .addClass('button')
         .html($me.html())
-		.on('keydown', {'plugin': plugin}, plugin.onKeyDown) //enable keyboard
+		.on('keydown', {'plugin': plugin}, plugin.onKeyDown) // enable keyboard navigation
         .on('click', {'plugin': plugin}, plugin.togglePanel);
         
-			$me.empty().append($btn); // wrap content of each header in an element with role button
+		$me.empty().append($btn); // wrap content of each header in an element with role button
 		});
 		
 		this.panels = $elem.children('dd').each(function(i, el) {
 			var $me = $(this), id = $elem.attr('id') + '_panel_' + i;
 			$me.attr({
-				'id': id
+				'id': id,
+				'role': 'region', // add role region to each panel
+				'aria-hidden': true, // mark all panels as hidden
+				'tabindex': 0 // add panels into the tab order
 			});
 		}).hide();
 		
 	};
+	
+	/**
+	     * Handles keydown event on header button.
+	     *
+	     * @param {Object} event - Keyboard event.
+	     * @param {object} event.data - Event data.
+	     * @param {object} event.data.plugin - Reference to plugin.
+	     */
+	    Plugin.prototype.onKeyDown = function (event) {
+	       
+	        var $me, $header, plugin, $elem, $current, ind;
+	       
+	        $me = $(event.target);
+	        $header = $me.parent('dt');
+	        plugin = event.data.plugin;
+	        $elem = $(plugin.element);
+			
+			console.log(event.keyCode);
+	       
+	        switch (event.keyCode) {
+	           
+	            // toggle panel by pressing enter key, or spacebar
+	            case ik_utils.keys.enter:
+	            case ik_utils.keys.space:
+	                event.preventDefault();
+	                event.stopPropagation();
+	                plugin.togglePanel(event);
+	                break;
+	           
+	            // use up arrow to jump to the previous header
+	            case ik_utils.keys.up:
+	                ind = plugin.headers.index($header);
+	                if (ind > 0) {
+	                    plugin.headers.eq(--ind).find('.button').focus();
+	                }
+	                console.log(ind);
+	                break;
+	           
+	            // use down arrow to jump to the next header
+	            case ik_utils.keys.down:
+	                ind = plugin.headers.index($header);
+	                if (ind < plugin.headers.length - 1) {
+	                    plugin.headers.eq(++ind).find('.button').focus();
+	                }
+					
+	                break;
+	        }
+	    };
 	
 	/** 
 	 * Toggles accordion panel.
@@ -69,46 +132,14 @@
 	 */
 	Plugin.prototype.togglePanel = function (event) {
 		
-		var plugin, $elem, $panel, $me, isVisible, $header, $current;
+		var plugin, $elem, $panel, $me, isVisible;
 		
 		plugin = event.data.plugin;
 		$elem = $(plugin.element);
 		$me = $(event.target);
-		$header = $me.parent('dt');
 		$panel = $me.parent('dt').next();
 		
-		isVisible = !!$panel.is(':visible');
-		$panel.slideToggle({ duration: plugin.options.animationSpeed });
-		//Added
-		switch (event.keyCode) {
-           
-            // toggle panel by pressing enter key, or spacebar
-            case ik_utils.keys.enter:
-            case ik_utils.keys.space:
-                event.preventDefault();
-                event.stopPropagation();
-                plugin.togglePanel(event);
-                break;
-           
-            // use up arrow to jump to the previous header
-            case ik_utils.keys.up:
-                ind = plugin.headers.index($header);
-                if (ind > 0) {
-                    plugin.headers.eq(--ind).find('.button').focus();
-                }
-                console.log(ind);
-                break;
-           
-            // use down arrow to jump to the next header
-            case ik_utils.keys.down:
-                ind = plugin.headers.index($header);
-                if (ind < plugin.headers.length - 1) {
-                    plugin.headers.eq(++ind).find('.button').focus();
-                }
-                break;
-        }
-		//
-		if(plugin.options.autoCollapse) { // collapse all other panels
+		if(plugin.options.autoCollapse) { // expand current panel and collapse the rest
 			
 			plugin.headers.each(function(i, el) {
 				var $hdr, $btn; 
@@ -117,10 +148,29 @@
 				$btn = $hdr.find('.button');
 				
 				if($btn[0] != $(event.currentTarget)[0]) { 
-					$btn.removeClass('expanded');
+					$btn.attr({'aria-expanded': false}).removeClass('expanded');
 					$hdr.next().slideUp(plugin.options.animationSpeed);
+				} else { 
+					$btn.attr({'aria-expanded': true}).addClass('expanded');
+					$hdr.next().slideDown(plugin.options.animationSpeed);
 				}
+				
+				$panel.attr({'aria-hidden': false});
 			});
+			
+		} else { // toggle current panel depending on the state
+		
+			isVisible = !!$panel.is(':visible');
+			if(isVisible) {
+			    $me.attr({'aria-expanded': false}).removeClass('expanded');
+			} else {
+			    $me.attr({'aria-expanded': true}).addClass('expanded');
+			}
+			
+			$panel.slideToggle({ duration: plugin.options.animationSpeed, done: function() {
+					$panel.attr({'aria-hidden': isVisible});
+				},
+			})
 			
 		}
 	};
